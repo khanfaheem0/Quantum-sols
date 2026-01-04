5ea3514c08e6bb7162603487415d99921ebcdc8f
Faheem <faheemk0077@gmail.com>
2026-01-05 02:35:56 +0530

    Senior Reset: App at root with HashRouter

--- App.jsx
+++ App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
-import Work from './pages/Work';
+import Services from './pages/Services';
 import Contact from './pages/Contact';
 
 function App() {
+  React.useEffect(() => {
+    if (!sessionStorage.getItem('sessionStart')) {
+      sessionStorage.setItem('sessionStart', Date.now().toString());
+    }
+  }, []);
+
   return (
     <Router>
       <div className="app-wrapper" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
@@ -16,7 +22,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/work" element={<Work />} />
+            <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
