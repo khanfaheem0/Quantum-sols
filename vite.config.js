a7ba10ed4e90f80b4c7ec9b34f428fbcc40a4ba3
Faheem <faheemk0077@gmail.com>
2026-01-05 02:15:13 +0530

    Senior Dev Cleanup: Moved app to root and deleted redundant folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Anti-Gravity/', // ADD THIS LINE (Use your exact Repo name here)
 })
