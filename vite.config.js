b62bad499a67975e8f382be11827ef0379871fdc
Faheem <faheemk0077@gmail.com>
2026-01-05 02:33:24 +0530

    Senior Reset: App at root with HashRouter

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This means "look in the same folder I'm in"
 })
