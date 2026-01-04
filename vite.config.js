759921638d53442cd958c8e13f689cc56662bffe
Faheem <faheemk0077@gmail.com>
2026-01-05 02:48:56 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This means "look in the same folder I'm in"
+  base: '/Quantum-sols/', // MUST match your Repo name exactly
 })
