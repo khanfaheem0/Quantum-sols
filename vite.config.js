a105e27ad931caf72624fba950165412674a1a9c
Faheem <faheemk0077@gmail.com>
2026-01-15 17:33:01 +0530

    chore: Update deployment base path and homepage URL.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Quantum-sols/', // MUST match your Repo name exactly
+  base: '/Quantumtechai/', // MUST match your Repo name exactly
 })
